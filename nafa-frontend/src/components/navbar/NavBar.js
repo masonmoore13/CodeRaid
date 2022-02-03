98a3a530a91382a51ce7400d3e86fdf869e54a2a
Sital Sitoula <heavywater221@gmail.com>
2022-02-02 22:30:02 -0600

    changed feature

--- nafa-frontend/src/components/navbar/NavBar.js
+++ nafa-frontend/src/components/navbar/NavBar.js
@@ -11,6 +11,7 @@ function NavBar(props) {
       <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarScroll" />
       <Navbar.Collapse id="navbarScroll">
+       
         <Nav
           className="me-auto my-2 my-lg-0"
           style={{ maxHeight: '100px' }}
@@ -19,11 +20,11 @@ function NavBar(props) {
           <Nav.Link href="#action1">Home</Nav.Link>
           <Nav.Link href="#action2">Link</Nav.Link>
           <NavDropdown title="Events" id="navbarScrollingDropdown">
-            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action5">
-              Something else here
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
             </NavDropdown.Item>
           </NavDropdown>
           <Nav.Link href="#">
