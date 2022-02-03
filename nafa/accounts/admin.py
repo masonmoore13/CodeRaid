646c1536ea7fee8faec471ce75d62e69e9a06db6
Bibek Bhandari <bibek09@outlook.com>
2022-02-03 15:58:38 -0600

    Customized User Model to custom fields

--- nafa/accounts/admin.py
+++ nafa/accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from .forms import *
 
-# Register your models here.
+
+# admin customization for the User model
+class UserAdmin(BaseUserAdmin):
+  form = UserChangeForm
+  fieldsets = (
+      (None, {'fields': ('email', 'password', )}),
+      (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+      (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                     'groups', 'user_permissions')}),
+      (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('user_info'), {'fields': ('address_line_1', 'city', 'state', 'have_paid_dues',  'phone_no')}),
+  )
+  add_fieldsets = (
+      (None, {
+          'classes': ('wide', ),
+          'fields': ('email', 'password1', 'password2', 'address_line_1', 'city', 'state', 'have_paid_dues'),
+      }),
+  )
+  list_display = ['email', 'first_name', 'last_name', 'is_staff', "phone_no"]
+  search_fields = ('email', 'first_name', 'last_name')
+  ordering = ('email', )
+admin.site.register(User, UserAdmin)
