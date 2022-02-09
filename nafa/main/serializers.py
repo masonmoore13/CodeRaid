f8bb770f7010bc8fe85124319b8749042c30df20
Bibek Bhandari <bibek09@outlook.com>
2022-02-09 17:23:37 -0600

    updated media files

---
+++ nafa/main/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from main.models import * 
+from .models import User
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'middle_name', 'last_name', 'password', 
+        'username', 'email', 'class_of', 'phone_no', 'have_paid_dues',
+        'current_work', 'has_contributions',  'achievements',
+        'address_line_1', 'city', 'state') 
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('id', 'rsvpd_members', 'event_name', 'date', 'location', 'banner_image', 
+        'gallery', 'description', 'media', 'registration_fees')
