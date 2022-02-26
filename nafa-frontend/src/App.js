e7b64ce1726dc601e3b01115895f4227b8654968
Sital Sitoula <heavywater221@gmail.com>
2022-02-25 19:52:15 -0600

    module not resolved fixed

--- nafa-frontend/src/App.js
+++ nafa-frontend/src/App.js
@@ -6,18 +6,18 @@ import PageNotFound from "./pages/404/404Page";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Home from "./pages/home/homePage/";
-import About from "./pages/about/aboutPage/";
-import Contact from "./pages/contact/contactPage/";
-import UserSignup from "./pages/user-signup/UserSignup/";
-import * as apiCalls from "./api/apiCalls/";
-import Login from "./pages/login/login/";
+import Home from "./pages/home/homePage";
+import About from "./pages/about/aboutPage";
+import Contact from "./pages/contact/contactPage";
+import UserSignup from "./pages/user-signup/UserSignup";
+import * as apiCalls from "./api/apiCalls";
+import Login from "./pages/login/login";
 
-import CreateEvent from "./pages/events/CreateEvent/";
-import ShowEvents from "./pages/events/ShowEvents/";
-import EventDetail from "./pages/events/EventDetail/";
-import EventUpdate from "./pages/events/EventUpdate/";
-import RouterWrapper from "./components/router-wrapper/RouterWrapper/";
+import CreateEvent from "./pages/events/CreateEvent";
+import ShowEvents from "./pages/events/ShowEvents";
+import EventDetail from "./pages/events/EventDetail";
+import EventUpdate from "./pages/events/EventUpdate";
+import RouterWrapper from "./components/router-wrapper/RouterWrapper";
 
 function App() {
   // will be changed later just for demo
@@ -34,14 +34,14 @@ function App() {
         <Routes>
           <Route element={<RouterWrapper />}>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/signup" element={<UserSignup actions={actions} />} />
+            <Route path="/about/" element={<About />} />
+            <Route path="/contact/" element={<Contact />} />
+            <Route path="/signup/" element={<UserSignup actions={actions} />} />
             <Route path="/login" element={<Login actions={actions} />} />
-            <Route exact path="/event" element={<ShowEvents />} />
-            <Route path="/event/createEvent" element={<CreateEvent />} />
+            <Route exact path="/event/" element={<ShowEvents />} />
+            <Route path="/event/createEvent/" element={<CreateEvent />} />
             <Route path="/event/:id/" element={<EventDetail />} />
-            <Route path="/event/:id/update" element={<EventUpdate />} />
+            <Route path="/event/:id/update/" element={<EventUpdate />} />
           </Route>
 
           <Route path="*" exact element={<PageNotFound />} />
