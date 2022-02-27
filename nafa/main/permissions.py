2b12952865c03d4b8c892e8fbbcc55ff108b9213
Bibek Bhandari <bibek09@outlook.com>
2022-02-27 16:34:12 -0600

    Fixed permissions

--- nafa/main/permissions.py
+++ nafa/main/permissions.py
@@ -4,24 +4,24 @@ class UserPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if view.action == 'list':
             return True
+        elif view.action == "retrieve" or view.action == "read":
+            return True
         elif view.action == 'create':
-            return request.user.is_authenticated and request.user.is_admin
-        elif view.action in ['retrieve', 'update', 'partial_update', 'destroy']:
-            return request.user.is_authenticated and request.user.is_admin
+            return request.user.is_authenticated and request.user.is_superuser
+        elif view.action in ['update', 'partial_update', 'destroy']:
+            return request.user.is_authenticated and request.user.is_superuser
         else:
             return False
 
                                                                                                 
     def has_object_permission(self, request, view, obj):
         # Deny actions on objects if the user is not authenticated
-        if not request.user.is_authenticated():
-            return False
 
         if view.action == 'retrieve':
-            return obj == request.user or request.user.is_admin
+            return True
         elif view.action in ['update', 'partial_update']:
-            return obj == request.user or request.user.is_admin
+            return obj == request.user or request.user.is_superuser
         elif view.action == 'destroy':
-            return request.user.is_admin
+            return request.user.is_superuser
         else:
             return False
