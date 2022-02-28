5e81e5717c8aa96a450770b51420f1463900fe11
Sital Sitoula <heavywater221@gmail.com>
2022-02-27 18:47:23 -0600

    recaptcha

---
+++ nafa-frontend/src/components/recaptcha/RecaptchaComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const RecaptchaComponent = () => {
+  return (
+    <ReCAPTCHA
+      sitekey="6LeyrKQeAAAAANzwYlkhakli2O2gIbC0dNHdM81p"
+      onChange={handleRecapthca}
+      theme="dark"
+    />
+  );
+};
