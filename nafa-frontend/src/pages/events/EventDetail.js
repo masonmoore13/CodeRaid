db2cfa874a8cf2a40f50a9eee8e0cc3e8925833c
Mason Moore <moorema@warhawks.ulm.edu>
2022-03-11 17:37:31 -0600

    Image modal

--- nafa-frontend/src/pages/events/EventDetail.js
+++ nafa-frontend/src/pages/events/EventDetail.js
@@ -7,6 +7,7 @@ import {
 } from "../../api/apiCalls";
 import { Card, CardGroup, Col, Modal, Button } from "react-bootstrap";
 import "./Event.css";
+import ModalImage from "react-modal-image";
 
 const EventDetail = () => {
   const [event, setEvent] = useState([]);
@@ -45,18 +46,18 @@ const EventDetail = () => {
     navigate.push("/");
   };
 
-  return (
-    <div className="container-md w-100 text-center ">
-      <img
-        className="container rounded"
-        src={event.banner_image}
-        alt="..."
-      ></img>
 
-      <Card className="container-md mx-auto mx-auto">
-        <div className="titleContainer d-flex flex-lg-wrap justify-content-center">
+  return (
+    <div className="text-center ">
+      <Card className="event-container container-md mx-auto">
+        <img
+          className="container banner-image"
+          src={event.banner_image}
+          alt="..."
+        ></img>
+        <div className="container d-flex flex-column flex-lg-wrap justify-content-center">
           <div className="eventName display-3">{event.event_name}</div>
-          <div className="deleteModal modal-fullscreen-sm-down">
+          <div className="deleteModal ">
             <Button
               className="btn btn-outline-dark btn-warning m-1"
               href={`/event/${event.id}/update`}
@@ -94,7 +95,7 @@ const EventDetail = () => {
             <Card
               className="detailsLeftCol mx-auto shadow-sm  "
               border=""
-              style={{ width: "96%" }}
+              style={{ width: "93%" }}
             >
               <p className="description m-2">{event.description}</p>
               <p className=" m-2"> {event.date}</p>
@@ -105,7 +106,7 @@ const EventDetail = () => {
           <Col className="DetailsRightCol m-2 col-lg-3 shadow-sm ">
             <Card
               className="venueCard m-3 shadow-sm text-start"
-              style={{ width: "93%" }}
+              style={{ width: "95%" }}
             >
               <Card.Title className="text-center">
                 Venue
@@ -125,29 +126,28 @@ const EventDetail = () => {
             <Card
               className="contactDetails m-3 shadow-sm "
               border=""
-              style={{ width: "93%" }}
+              style={{ width: "95%" }}
             >
               <Card.Title>
                 Contact Details <hr />
               </Card.Title>
-              <h5> {event.contact_name}</h5>
-              <h6> {event.contact_email}</h6>
-              <h6> {event.contact_number}</h6>
+              <h5 className="text-start">{event.contact_name}</h5>
+              <h6 className="text-start"> {event.contact_email}</h6>
+              <h6 className="text-start"> {event.contact_number}</h6>
             </Card>
             <Card
-              className="DetailsRightCol m-3 shadow-md "
+              className="DetailsRightCol m-3 shadow-sm "
               border=""
-              style={{ width: "93%" }}
+              style={{ width: "95%" }}
             >
               <Card.Title>
                 Event Pictures <hr />
                 <div className="showGallery d-flex flex-wrap m-3">
                   {gallery.map((gallery, index) => (
-                    <div key={gallery.id}>
-                      <img
-                        src={gallery.images}
-                        alt="event images"
-                        style={{ width: "6em" }}
+                    <div AppImagekey={gallery.id}>
+                      <ModalImage
+                        small={gallery.images}
+                        large={gallery.images}
                       />
                     </div>
                   ))}
